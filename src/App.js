f706c12b95b3f39d6d993c85f061a7b4911b7c66
Sharjeel-1999 <67510731+Sharjeel-1999@users.noreply.github.com>
2022-03-16 19:58:23 +0500

    create-basic-comp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SubCom from './comp/SubCom';
+
+function App() {
+  return (
+    <div>
+     <SubCom />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/SubCom.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+function SubCom(){
+  return (
+    <div className='main-second-container'>
+
+ <h1 className='st-cont'>Hello World</h1>
+ <h1 className='sst-cont'>first component</h1>
+     
+    </div>
+  );
+}
+
+export default SubCom;
